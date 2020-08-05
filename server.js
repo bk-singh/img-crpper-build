62ad902107d05d096a73f5a24afe8eeb9039da76
Bal Krishn Singh <balkrishn.singh@sojern.com>
2020-08-05 09:05:58 +0530

    image cropper server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var body_parser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 60000;
 
 app.use(express.static("public"));
 app.use(cors());
