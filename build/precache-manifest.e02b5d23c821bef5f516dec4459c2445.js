652675e3cfadaf354379d0143545c18f5d35db49
Bal Krishn Singh <balkrishn.singh@sojern.com>
2020-08-05 09:05:58 +0530

    image cropper server

---
+++ build/precache-manifest.e02b5d23c821bef5f516dec4459c2445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdfe2c6270284d187971d700f9f87d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa23f7d3f3c34c8b824",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1bdc16983b61bdc465f0",
+    "url": "/static/js/2.8a572a67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a572a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa23f7d3f3c34c8b824",
+    "url": "/static/js/main.7b3f3f1b.chunk.js"
+  },
+  {
+    "revision": "21425612717fddde26a5",
+    "url": "/static/js/runtime-main.dc563875.js"
+  }
+]);
